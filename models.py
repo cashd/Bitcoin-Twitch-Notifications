3684c36131c113ec31d882f4572aceec234a2fb7
Cash DeLeon <cashadeleon@gmail.com>
2017-09-27 00:27:10 -0500

    started models

--- models.py
+++ models.py
@@ -1,2 +1,18 @@
-import peewee
+from peewee import *
 
+db = SqliteDatabase('database.db')
+
+class User(Model):
+	uid = 
+	email = CharField()
+	twitch_id = CharField()
+	btc_address = CharField()
+
+	class Meta:
+		database = db
+
+
+
+class Transaction(Model):
+	user = ForeignKeyField(User, related_name="transactions")
+	
