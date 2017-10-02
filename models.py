5f4a9d46f62fbd151f62b656f14d65988b48a2f8
Cash DeLeon <cashadeleon@gmail.com>
2017-10-02 14:56:47 -0500

    added model fields

--- models.py
+++ models.py
@@ -3,10 +3,14 @@ from peewee import *
 db = SqliteDatabase('database.db')
 
 class User(Model):
-	uid = 
+	# uid = 
 	email = CharField()
 	twitch_id = CharField()
+	twitch_username = CharField()
 	btc_address = CharField()
+	twitch_profile_picture = CharField()
+	profile_bio = CharField()
+
 
 	class Meta:
 		database = db
@@ -15,4 +19,15 @@ class User(Model):
 
 class Transaction(Model):
 	user = ForeignKeyField(User, related_name="transactions")
+	rec_wallet_address = CharField()
+	display_name = CharField()
+	display_msg = CharField()
+	#amount = IntegerField()
+	currency_type = CharField()
+
+	class Meta:
+		database = db
+
+
+
 	
