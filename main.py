98bb992b83099de7a3e76b2ec6e47d94c35ac06b
Cash DeLeon <cashadeleon@gmail.com>
2017-10-31 17:58:35 -0500

    auth

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import tornado.ioloop
 import tornado.web
+import settings
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+class Application(tornado.web.Application):
+	def __init__(self):
+		handlers = [
+		(r"/", MainHandler),
+		(r"/auth/login/", AuthLoginHandler), 
+		(r"/auth/logout", AuthLogoutHandler),
+		]
+
+		settings = {
+			"template_path": settings.TEMPLATE_PATH,
+			"static_path": settings.STATIC_PATH,
+			"debug": settings.DEBUG,
+			"cookie_secret": settings.COOKIE_SECRET,
+			"login_url": "/auth/login/"
+		}
 
 if __name__ == "__main__":
     app = make_app()
